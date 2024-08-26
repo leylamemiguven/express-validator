19bce58f126330820661cb6a2bd7ed5f7c028f30
Leyla Memiguven <leyla.memiguven@mail.utoronto.ca>
2024-08-26 10:24:19 -0400

    Adding styling

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 // app.js
 const express = require("express");
+const path = require('path');
 const app = express();
 const usersRouter = require("./routes/usersRouter");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", usersRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
